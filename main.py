1ec62e99902a0770d5dedbd578c108d4192956f0
Tushar Jain <tusharlock10@gmail.com>
2019-03-04 16:30:54 +0530

    Add command-line interface in main.py

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import sys, dictionary
 
-
+ALL_ARGS=['--ADD', '-A', '--SEARCH', '-S']
 args=sys.argv[1:]
 
-word=''.join(args)
+if args[-1] in ALL_ARGS:
+    arg=args[-1]
+else:
+    arg='-S'
 
-dictionary.search_word(word)
+word=''.join(args.remove(arg))
+
+    
+if arg in ['--ADD','-A']:
+    dictionary.add_new_word()
+else:
+    dictionary.search_word(word)
